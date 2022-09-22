979f7f4cc534f0d58088267e272c6fac87bd1b76
Elgynart <elgynart@gmail.com>
2022-09-22 18:26:49 +0300

    made sort friendlist

--- index.js
+++ index.js
@@ -1,15 +1,62 @@
-const results = 10;
-const seed = 'abc';
-let page = 1;
+let defaultFriendList = [];
+let friendList = [];
 
-const friendsUrl = `https://randomuser.me/api/?results=${results}&seed=${seed}&page=${page}`;
-const body = document.querySelector('body');
+loadFriendsData();
 
-fetch(friendsUrl)
-.then(response => response.json())
-.then(data => {
-    //body.innerText = JSON.stringify(data.results[0].picture.large);
-    body.innerHTML = '<img src="' + data.results[0].picture.large + '">';
-    body.innerHTML += '<img src="' + data.results[0].picture.medium + '">';
-    body.innerHTML += '<img src="' + data.results[0].picture.thumbnail + '">';
-});
+function loadFriendsData() {
+    const SEED = 'abc';
+    const results = 10;
+    const listOfMonth = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+    const url = `https://randomuser.me/api/?results=${results}&seed=${SEED}&exc=login,id`;
+    
+    fetch(url)
+    .then(response => {
+        if(!response.ok) return Promise.reject(response.text());
+
+        return response.json();
+    })
+    .then(data => {
+        defaultFriendList = data.results.reduce((allValues, currValue, id) => {
+            const { gender, name:{title, first, last}, location:{country}, email, dob:{date, age}, picture, registered:{date:registeregDate} } = currValue;
+
+            const [year, month, day] = date.split('T')[0].match(/[0-9]{2,4}/g);
+            const fulldate = `${day} ${listOfMonth[+month - 1]} ${year}`;
+            const fullname = first + ' ' + last;
+            const [yearReg, monthReg, dayReg] = registeregDate.split('T')[0].split('-');
+            const fullRegDate = dayReg + '.' + monthReg + '.' + yearReg;
+
+            allValues.push({
+                id,
+                gender,
+                name: {title, fullname},
+                country,
+                email,
+                dob: {fulldate, age},
+                registered: {fullregistered: fullRegDate, year: yearReg, month: monthReg, day: dayReg},
+                picture,
+            });
+            
+            return allValues;
+        }, []);
+
+        friendList = defaultFriendList.slice();
+
+        createFriendCard();
+    })
+    .catch(error => {
+        error.then(data => console.log(data));
+    });
+}
+
+function createFriendCard() {
+    console.log(JSON.stringify(friendList));
+}
+
+function paginationTemplate() {
+    
+}
+
+function friendCardTempalte() {
+
+}
