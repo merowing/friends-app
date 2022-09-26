38d7bd50ba334f9d85c12817b2ad716c58998c68
Elgynart <elgynart@gmail.com>
2022-09-26 23:18:02 +0300

    mobile support

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 let defaultFriendList = [];
 let friendList = [];
 
+const mobileCheck = function() {
+    let check = false;
+    (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+    return check;
+};
+const isMobile = mobileCheck();
+
 const search = document.querySelector(".search");
 const searchClear = document.querySelector(".search-clear");
 const friendsNotFoundBlock = document.querySelector('.friends-not-found');
@@ -8,6 +15,9 @@ const selectAgeRanges = document.querySelectorAll('.filter-ages > select');
 
 const perpage = 5;
 const pagination = document.querySelector('.pagination > ul');
+
+const mobileSearch = document.querySelector('.mobile-search > input');
+
 let paginationTabs;
 let activePage = 0;
 let activeGender = 'both';
@@ -64,8 +74,8 @@ function loadFriendsData() {
 
         //createFriendCard();
         createAgeRange();
-        createPagination();
         createCountryFilter();
+        createPagination();
     })
     .catch(error => {
         console.log(error);
@@ -131,7 +141,7 @@ function createPagination() {
     pagination.appendChild(tabs);
 
     paginationTabs = pagination.querySelectorAll('li');
-
+alert(filters);
     console.log(createFilters());
     friendList = friendList.slice(activePage * perpage, (activePage + 1) * perpage);
     
@@ -233,7 +243,9 @@ function createFilters() {
         return params;
     }, []);
 
-    const searchValue = filters.splice(0, 1);
+    let searchValue = filters.splice(0, 1)[0];
+    if(isMobile) searchValue = mobileSearch.value;
+    searchValue = searchValue.toLowerCase();
 
     const selectedCountry = [...countryBlock.querySelectorAll('option')].reduce((countries, option) => {
         if(option.selected) countries.push(option.value);
@@ -244,7 +256,7 @@ function createFilters() {
         return age.options[age.selectedIndex].text;
     });
 
-    return [...filters, selectedCountry, ageRange, ...searchValue];
+    return [...filters, selectedCountry, ageRange, searchValue];
 }
 
 function filterFriendList(filters) {
@@ -312,7 +324,7 @@ function filterFriendList(filters) {
 
         }
 
-        if(index === 3 && filter.indexOf('-1') === -1) {
+        if(index === 3 && filter.indexOf('-1') === -1 && filter.length > 0) {
             console.log(1, friendList);
             filter = filter.map(country => country.toLowerCase());
             friendList = friendList.filter(friend => filter.indexOf(friend.country.toLowerCase()) !== -1);
@@ -333,7 +345,7 @@ function filterFriendList(filters) {
     return friendList;
 }
 
-search.addEventListener("keyup", (e) => {
+search.addEventListener("keyup", () => {
     clearInterval(searchTimer);
     searchPressed = true;
 
@@ -386,12 +398,24 @@ pagination.addEventListener("click", e => {
 
     friendList = filterFriendList(createFilters());
     friendList = friendList.splice(perpage * page, perpage);
+
+    window.scrollTo(0, 0);
     
     createFriendCard();
 });
 
 function createCountryFilter() {
     const countryOptionElement = document.createDocumentFragment();
+
+    // mobile
+    if(!isMobile) {
+        const firstElem = document.createElement('option');
+        firstElem.value = "-1";
+        firstElem.text = "Select all";
+        firstElem.setAttribute("selected", "");
+        countryBlock.appendChild(firstElem);
+    }
+    // mobile
 
     countries.sort();
     countries.forEach(item => {
@@ -402,10 +426,13 @@ function createCountryFilter() {
     });
     
     countryBlock.appendChild(countryOptionElement);
+
+
 }
 
 let countryBlockOptionsChecked = [];
 countryBlock.addEventListener("mousedown", e => {
+    if(!isMobile) {
     e.preventDefault();
     const options = countryBlock.querySelectorAll('option');
     const index = [...options].indexOf(e.target);
@@ -416,7 +443,7 @@ countryBlock.addEventListener("mousedown", e => {
         });
     }else {
         if(options[0].selected) options[0].selected = false;
-        
+    
         e.target.selected = !e.target.selected;
         // if(!e.target.selected)
         //     e.target.setAttribute('selected', '');
@@ -430,6 +457,11 @@ countryBlock.addEventListener("mousedown", e => {
     
     scrollTop = countryBlock.scrollTop;
     setTimeout(() => countryBlock.scrollTo(0, scrollTop), 0);
+    }
+    // options[0].selected = false;
+    // options[0].removeAttribute("selected");
+    // options[1].addAttribute("selected", "selected");
+    // options[1].addAttribute("selected", true);
 
     //activePage = 0;
     //console.log(createFilters());
@@ -443,12 +475,6 @@ countryBlock.addEventListener("mousedown", e => {
 });
 countryBlock.addEventListener("click", e => {
     if(e.target.tagName === 'OPTION') {
-        // friendList = filterFriendList(createFilters());
-
-        // if(friendList.length / perpage <= activePage) {
-        //     activePage = 0;
-        // }
-        
         createPagination();
     }
 });
@@ -473,3 +499,27 @@ function createAgeRange() {
 
     selectAgeRanges.forEach((select, ind) => select.appendChild(optionsFragment(!!ind)));
 }
+
+// mobile
+//const mobileApplyButton = document.querySelector('.mobile-apply');
+const mobileMenu = document.querySelector('.mobile-menu');
+const mobileMenuClose = document.querySelector('.mobile-close');
+const filter = document.querySelector('.filter');
+
+mobileMenu.addEventListener("click", () => {
+    filter.classList.add('visible');
+    document.body.classList.add('overflow');
+});
+mobileMenuClose.addEventListener("click", () => {
+    filter.classList.remove('visible');
+    document.body.classList.remove('overflow');
+});
+mobileSearch.addEventListener("keyup", () => {
+    friendList = filterFriendList(createFilters());
+
+    if(friendList.length / perpage <= activePage) {
+        activePage = 0;
+    }
+
+    createPagination();
+});
